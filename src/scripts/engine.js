147271024205d7f86c180662e55e13a40116c4db
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-12-05 17:36:22 -0300

    create showHideKeys function

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,8 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key')
+const volumeSlider = document.querySelector('.volume-slider input')
+const keysCheck = document.querySelector('.keys-check input')
 
+const mapedKeys = []
 let audio = new Audio('src/tunes/a.wav')
 
 const playTune = (key) => {
@@ -15,9 +18,25 @@ const playTune = (key) => {
 
 pianoKeys.forEach(key => {
     key.addEventListener('click', () => playTune(key.dataset.key))
+    mapedKeys.push(key.dataset.key)
 })
 
 document.addEventListener("keydown", (event) => {
-    playTune(event.key)
-    console.log(event)
+    if(mapedKeys.includes(event.key)) {
+        playTune(event.key)
+    }
 })
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value
+}
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => {
+        key.classList.toggle('hide')
+    })
+}
+
+volumeSlider.addEventListener('input', handleVolume)
+
+keysCheck.addEventListener('click', showHideKeys)
